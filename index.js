9ba73a69ea059c8b0d3f02bad4b26c34e6b79ce9
Sunethia <lawrencesunethia@gmail.com>
2022-09-05 16:32:09 +0200

    changes

--- index.js
+++ index.js
@@ -11,26 +11,20 @@ app.get("/", (req, res) => {
   res.json({ msg: "Welcome" });
 }); // home route
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 const userRoute = require("./routes/userRoute");
 app.use("/users", userRoute);
 
 const productsRoute = require("./routes/productRoute");
 app.use("/products", productsRoute);
 
-// app.get("/", function (req, res) {
-//   res.sendFile(_dirname + "/" + "login.html");
-// });
-// app.get("/", function (req, res) {
-//   res.sendFile(_dirname + "/" + "register.html");
-// });
-// app.get("/", function (req, res) {
-//   res.sendFile(_dirname + "/" + "reset_password.html");
-// });
-
-// app.listen(app.get("port"), () => {
-//   console.log(`Listening for calls on port ${app.get("port")}`);
-//   console.log("Press Ctrl+C to exit server");
-// });//listening for API calls
 const port = process.env.PORT || 6969;
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
